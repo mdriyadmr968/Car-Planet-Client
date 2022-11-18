5a411b04608751507040f3597dd47cc800be3fe3
mdriyadmr968 <mdriyadmr968@gmail.com>
2022-11-18 17:29:37 +0600

    review updated

--- src/Components/Homepage/Home/Home.js
+++ src/Components/Homepage/Home/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Loadcars from "../Loadcars/Loadcars";
+import Reviews from "../Reviews/Reviews";
 import WhatAChieve from "../WhatAChieve/WhatAChieve";
-// import Limitedoffer from "../Limitedoffer/Limitedoffer";
+import Limitedoffer from "../Limitedoffer/Limitedoffer";
 import Whyus from "../Whyus/Whyus";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      {/* <Limitedoffer></Limitedoffer> */}
+      <Limitedoffer></Limitedoffer>
       <Whyus></Whyus>
       <WhatAChieve></WhatAChieve>
       <Loadcars></Loadcars>
+      <Reviews></Reviews>
     </div>
   );
 };
--- src/Components/Homepage/Reviews/Reviews.js
+++ src/Components/Homepage/Reviews/Reviews.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Rating from "react-rating";
+import reviewImage from '../../../Images/travelers1.png'
+import { DynamicStar } from "react-dynamic-star";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Reviews.css";
+import Rating from "react-rating";
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
@@ -11,81 +15,38 @@ const Reviews = () => {
   }, []);
 
   return (
-    <>
-      <h2 className="container text-center text-warning">
-        Top reviews of customers
-      </h2>
-      <div
-        className=" container w-50 my-5 slider"
-        style={{ backgroundColor: "salmon" }}
-      >
-        <div
-          id="carouselExampleInterval"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-inner text-center">
-            {reviews.map((review, index) => {
-              return (
-                <div
-                  className={
-                    index === 0 ? "carousel-item active" : "carousel-item"
-                  }
-                  data-bs-interval="2000"
-                  key={review._id}
-                >
-                  <div className="d-flex flex-column">
-                    <div className="w-100 py-5">
-                      <img
-                        src={review.img}
-                        className="img-fluid  round-img  center"
-                        alt=""
-                      />
-                    </div>
-                    <h4 className="text-light customer-name">{review.name}</h4>
-                    <h4>
-                      <Rating
-                        initialRating={review.rating}
-                        emptySymbol="far fa-star icon-color"
-                        fullSymbol="fas fa-star icon-color"
-                        readonly
-                      ></Rating>
+    <div className="review-wrapper">
+      <div className="review-container">
+        <h2 className="review-title">Top Review</h2>
+        <p className="review-description">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+          architecto tempore veritatis tempora. Dolores!{" "}
+        </p>
+
+        <div className="review-card-container">
+          {reviews.map((review)=>{
+            return (
+              <div className="review-card" key={review._id}>
+                <div className="review-top">
+                  <div>
+                    <img src={reviewImage} className="reviewer-img" alt="" />
+                  </div>
+                  <div>
+                    <h3 className="reviewer">{review.name}</h3>
+                    <h4 className="dynamic-star">
+                      <DynamicStar rating={review.rating} width={"15"} />
                     </h4>
-                    <p className="text-light px-5 text-center pb-5 review">
-                      {review.review}
-                    </p>
                   </div>
                 </div>
-              );
-            })}
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleInterval"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon "
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleInterval"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon "
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+                <div className="review-bottom">
+                  <p>{review.review}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
